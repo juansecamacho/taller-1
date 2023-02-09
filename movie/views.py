3e92b969088204bdac3c96d0cd34030bcc52e8db
juansecamacho <jscamachop13@gmail.com>
2023-02-09 12:24:38 -0500

    primer intento de la creacion de la pagina about

--- movie/views.py
+++ movie/views.py
@@ -7,3 +7,6 @@ def home(request):
    # return HttpResponse('<h1>Welcome to home page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Sebastian Camacho'})
+
+def about(request):
+    return render(request, 'about.html')
